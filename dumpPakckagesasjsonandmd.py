8217385b5688a10e86802d90dae6f1af3d0f8106
DeX77 <dex@dragonslave.de>
2023-11-14 22:31:48 +0100

    * don't bail out early

--- dumpPakckagesasjsonandmd.py
+++ dumpPakckagesasjsonandmd.py
@@ -62,86 +62,84 @@ while i :
     full_pkg = pacman.PKGp_new()
     package_path= packages_path + name + "-" + version + "-" + arch + ".fpm"
 
-    if pacman.pkg_load(package_path, full_pkg) == -1:
-        i = pacman.list_next(i)
-    files = []
-    licenses = []
+    if pacman.pkg_load(package_path, full_pkg) != -1:
+        files = []
+        licenses = []
 
-    m = pacman.void_to_PM_LIST(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_FILES))
-    while m:
-        package_file = pacman.void_to_char(pacman.list_getdata(m))
-        files.append(package_file)
-        m = pacman.list_next(m)
+        m = pacman.void_to_PM_LIST(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_FILES))
+        while m:
+            package_file = pacman.void_to_char(pacman.list_getdata(m))
+            files.append(package_file)
+            m = pacman.list_next(m)
 
-    n = pacman.void_to_PM_LIST(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_LICENSE))
-    while n:
-        license = pacman.void_to_char(pacman.list_getdata(n))
-        licenses.append(license)
-        n = pacman.list_next(n)
-    url = pacman.void_to_char(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_URL))
-    builddate = pacman.void_to_char(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_BUILDDATE))
-
-
-#    package_as_dict = {
-#        'name' : name,
-#        'version' : version ,
-#        'desc' : pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_DESC)).decode('latin-1').encode("utf-8") ,
-#        'groups' : groups,
-#        'url' : url ,
-#        'arch' : arch ,
-#        'builddate' : pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_BUILDDATE)) ,
-#        'size' : pacman.void_to_long(pacman.pkg_getinfo(pkg, pacman.PKG_SIZE)) ,
-#        'usize' : pacman.void_to_long(pacman.pkg_getinfo(pkg, pacman.PKG_USIZE)) ,
-#        'sha1sums' : pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_SHA1SUM)) ,
-#        'depends' : depends ,
-#        'license' : licenses,
-#        'files' : files
-#    }
-#    packages_as_dict.append(package_as_dict)
+        n = pacman.void_to_PM_LIST(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_LICENSE))
+        while n:
+            license = pacman.void_to_char(pacman.list_getdata(n))
+            licenses.append(license)
+            n = pacman.list_next(n)
+        url = pacman.void_to_char(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_URL))
+        builddate = pacman.void_to_char(pacman.pkg_getinfo(pacman.PKGp_to_PKG(full_pkg), pacman.PKG_BUILDDATE))
 
 
-    packages_as_dict[name] = {
-        'name': name,
-        'version' : version ,
-        'desc' : desc ,
-    }
-    if files:
-        packages_as_dict[name]['files'] = files
-    if url:
-        packages_as_dict[name]['url'] = url
-    if builddate:
-        packages_as_dict[name]['builddate'] = builddate
-    if licenses:
-        packages_as_dict[name]['licenses'] = licenses
+    #    package_as_dict = {
+    #        'name' : name,
+    #        'version' : version ,
+    #        'desc' : pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_DESC)).decode('latin-1').encode("utf-8") ,
+    #        'groups' : groups,
+    #        'url' : url ,
+    #        'arch' : arch ,
+    #        'builddate' : pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_BUILDDATE)) ,
+    #        'size' : pacman.void_to_long(pacman.pkg_getinfo(pkg, pacman.PKG_SIZE)) ,
+    #        'usize' : pacman.void_to_long(pacman.pkg_getinfo(pkg, pacman.PKG_USIZE)) ,
+    #        'sha1sums' : pacman.void_to_char(pacman.pkg_getinfo(pkg, pacman.PKG_SHA1SUM)) ,
+    #        'depends' : depends ,
+    #        'license' : licenses,
+    #        'files' : files
+    #    }
+    #    packages_as_dict.append(package_as_dict)
 
+
+        packages_as_dict[name] = {
+            'name': name,
+            'version' : version ,
+            'desc' : desc ,
+        }
+        if files:
+            packages_as_dict[name]['files'] = files
+        if url:
+            packages_as_dict[name]['url'] = url
+        if builddate:
+            packages_as_dict[name]['builddate'] = builddate
+        if licenses:
+            packages_as_dict[name]['licenses'] = licenses
+
+        f = open(args.hugo_directory+'content/package/'+name+'.md', 'w')
+            f.write("+++\n")
+            f.write("draft = false\n")
+            f.write('title = "'+name+" "+version+'"\n')
+            f.write('version = "'+version+'"\n')
+            if builddate:
+                date_object = datetime.datetime.strptime(builddate, '%c')
+                f.write('date = "'+date_object.isoformat()+'"\n')
+
+            f.write('categories = '+str(groups)+'\n')
+            if url:
+                f.write('upstreamurl = "'+url+'"\n')
+            f.write('arch = "'+arch+'"\n')
+            f.write('size = "'+str(size)+'"\n')
+            f.write('usize = "'+str(usize)+'"\n')
+            f.write('sha1sum = "'+sha1sum+'"\n')
+            f.write('depends = "'+str(depends)+'"\n')
+            if licenses:
+                f.write('license = "'+str(license)+'"\n')
+        #    if files:
+        #        f.write('files = "'+str(files)+'"\n')
+            f.write("+++\n")
+            f.write(desc)
+            f.close()
 
     i = pacman.list_next(i)
 
-
-    f = open(args.hugo_directory+'content/package/'+name+'.md', 'w')
-    f.write("+++\n")
-    f.write("draft = false\n")
-    f.write('title = "'+name+" "+version+'"\n')
-    f.write('version = "'+version+'"\n')
-    if builddate:
-        date_object = datetime.datetime.strptime(builddate, '%c')
-        f.write('date = "'+date_object.isoformat()+'"\n')
-
-    f.write('categories = '+str(groups)+'\n')
-    if url:
-        f.write('upstreamurl = "'+url+'"\n')
-    f.write('arch = "'+arch+'"\n')
-    f.write('size = "'+str(size)+'"\n')
-    f.write('usize = "'+str(usize)+'"\n')
-    f.write('sha1sum = "'+sha1sum+'"\n')
-    f.write('depends = "'+str(depends)+'"\n')
-    if licenses:
-        f.write('license = "'+str(license)+'"\n')
-#    if files:
-#        f.write('files = "'+str(files)+'"\n')
-    f.write("+++\n")
-    f.write(desc)
-    f.close()
 
 with open(args.hugo_directory+'static/js/lunr/PagesIndex.json', 'w') as f:
     json.dump(list(packages_as_dict.values()), f, sort_keys=True, indent=4 * ' ')
